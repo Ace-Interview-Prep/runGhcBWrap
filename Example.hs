dfd2f81caa4d8e08b988c899f09d076e6f315791
lazyLambda <galen.sprout@gmail.com>
2025-11-27 22:16:08 -0500

    Add example

---
+++ Example.hs
@@ -0,0 +1,40 @@
+
+import RunGhcBWrap
+
+main :: IO ()
+main = do
+  testRunGhcBWrap $ unlines
+    [ "{-# LANGUAGE OverloadedStrings #-}"
+    , "module Main where"
+    , "import System.FilePath"
+    , "import System.Directory"
+    , "import Data.Text"
+    , "import qualified Data.Text as T"
+    , "import Data.ByteString"
+    , "import qualified Data.ByteString.Char8 as BS"
+    , "import Data.Map"
+    , "import qualified Data.Map as Map"
+    , "import Data.Vector (Vector)"
+    , "import qualified Data.Vector as V"
+    , "import Data.Time.Clock (getCurrentTime)"
+    , "import System.Directory (listDirectory)"
+    , "import System.FilePath ((</>))"
+    , "import System.Process (readProcess)"
+    , "import Data.Aeson (encode, object, (.=))"
+    , "import System.Random (randomRIO)"
+    , "import System.Environment"
+    , "import Control.Exception (try, SomeException)"
+    , "import Control.Monad (forM_)"
+    , "import Control.Monad.State (State, evalState, put, get)"
+    , ""
+
+    , "main :: IO ()"
+    , "main = do"
+    -- , "  print =<< getEnv \"PATH\""
+
+    , "  putStrLn \"holy meta mate\"   "
+    , "  Prelude.appendFile \"example.txt\" \"im in a file\""
+    , "  Prelude.appendFile \"example.txt\" \"im in a file\""
+    , "  x <- Prelude.readFile \"example.txt\" "
+    , "  putStrLn $ \"haskell readFile contents: \" <> x"
+    ]
