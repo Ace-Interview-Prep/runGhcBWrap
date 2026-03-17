83aadfa4fd78d2a30b68abb092043cbb356f7cb1
lazyLambda <galen.sprout@gmail.com>
2026-03-17 00:50:15 -0400

    Fix default.nix: remove relative ../runGhcBWrap-core path, use function arg

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ let
   pkgs_unstable = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
   }) {};
-  #inherit (nixpkgs) pkgs;
+
   n_ = pkgs.fetchFromGitHub {
     owner = "obsidiansystems";
     repo = "nix-thunk";
@@ -16,28 +16,18 @@ let
   };
   n = import n_ {};
   sources = n.mapSubdirectories n.thunkSource ./thunks;
-  runGhcBWrap-core_ = pkgs.haskell.lib.doJailbreak (pkgs.haskellPackages.callCabal2nix "runGhcBWrap-core" ../runGhcBWrap-core {});
-  IStr_ = pkgs.haskellPackages.callCabal2nix "IStr" sources.IStr {};
-  scrappy-core_ = pkgs.haskellPackages.callCabal2nix "scrappy-core" sources.scrappy-core {};
-
-  ghc_unstable = pkgs_unstable.haskell.packages.ghc912;
 
   overrides_ = pre: post: {
-    runGhcBWrap-core = pkgs.haskell.lib.doJailbreak (post.callCabal2nix "runGhcBWrap-core" ../runGhcBWrap-core {});
+    runGhcBWrap-core = runGhcBWrap-core;
     IStr = pre.callCabal2nix "IStr" sources.IStr {};
     scrappy-core = pre.callCabal2nix "scrappy-core" sources.scrappy-core {};
   };
-  
+
   ghc_9_12 = (pkgs_unstable.haskell.packages.ghc912.override { overrides = overrides_; }).ghcWithPackages (
     hpkgs: with hpkgs; [
       temporary vector aeson parsec hpkgs.runGhcBWrap-core hpkgs.IStr hpkgs.scrappy-core
     ]
   );
-  # ghc_9_12 = ghc_unstable.ghcWithPackages (
-  #   hpkgs: with hpkgs; [
-  #     temporary vector aeson parsec runGhcBWrap-core_ IStr_ scrappy-core_
-  #   ]
-  # );
 in
 mkDerivation {
   pname = "runGhcBWrap";
